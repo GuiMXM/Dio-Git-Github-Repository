b91f021631ed62887270e30f820723d6921545b4
Guimxm <gui2918@gmail.com>
2022-08-08 19:00:13 -0300

    ProjetoFinalJAVA

---
+++ Projeto JAVA/InterfaceConta.java
@@ -0,0 +1,13 @@
+package moduloJava.projetobanco;
+
+public interface IConta {
+    void transferir(double valor, Conta contaDestino);
+
+    void depositar(double valor);
+
+    void sacar(double valor);
+
+    void imprimirExtrato();
+}
+Footer
+© 2022 GitHub, In
